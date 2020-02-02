43af8aac82bf36f60d6d8bc92f402880cde54942
Yuta Nakayama <nkymut@gmail.com>
2020-02-02 18:19:03 +0800

    1st commit (README, SendRGB)

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # microbit-webusb-p5js
 Example files for interacting with micro:bit and p5.js over WebUSB
+
+# References 
+*( [bsiever/microbit-webusb] https://github.com/bsiever/microbit-webusb):WebUSB Utils for Micro:bit (( [live DEMO] https://bsiever.github.io/microbit-webusb/))
+* ([micro:bit](https://microbit.org/))
+* ([p5*js](https://p5js.org/))
+
+# Overall Setup
+
+1. Upgrade Micro:bit to latest firmware (above version 249) [Updating your micro:bit firmware
+](https://microbit.org/guide/firmware/)
+
+2. Program the Micro:bit with one of the example programs that generates serial data
+3. Setup a Web Server & Open the project's page to run the sample application ([`index.html`](./index.html)) ([Live Example of it](https://github.com/bsiever/microbit-webusb))
+
+# Examples
+
+* Control Neopixel
+** ([p5*js code] (https://editor.p5js.org/didny/sketches/cTFjpyKd))
+** ([micro:bit code](https://makecode.microbit.org/_c5iHFsERyPhr) )
---
+++ examples/SendRGB/sketch.js
@@ -0,0 +1,141 @@
+/*
+ * @name WebUSB RGB Control
+ * @description This example sends and receives multiple values to micro:bit over WebUSB. 
+ *  Using microbit-webusb library. https://github.com/bsiever/microbit-webusb
+ */
+let connectBtn;
+
+let CONSOLE_LOG = true;
+
+// List of connected devices (a single value could be used if only connecting to one device)
+let connectedDevice;
+
+let sliderR;
+let sliderG;
+let sliderB;
+
+let brightness = 0;
+let sendText = "sent:";
+let recvText = "recv:";
+
+let R = 0;
+let G = 0;
+let B = 0;
+
+
+function setup() {
+  createCanvas(400, 400);
+  connectBtn = createButton("connect");
+  connectBtn.position(20,height-30);
+  connectBtn.mousePressed(connect);
+  connectBtn.style("width:360px");
+  // 
+  sliderR = createSlider(0,255);
+  sliderR.position(width/2 - 175, 10);
+  sliderR.changed(sliderInput);
+  sliderR.style("width:350px");
+
+  // 
+  sliderG = createSlider(0,255);
+  sliderG.position(width/2 - 175, 35);
+  sliderG.changed(sliderInput);
+  sliderG.style("width:350px");
+  
+  // 
+  sliderB = createSlider(0,255);
+  sliderB.position(width/2 - 175, 60);
+  sliderB.changed(sliderInput);
+  sliderB.style("width:350px");
+}
+
+function draw() {
+  background(color(R,G,B));
+  fill(255);
+  textSize(32);
+  text(sendText,20,100);
+  text(recvText,20,125);
+  
+  //colorMode(HSB, 255);
+  
+  // ellipse(r,g,b);
+}
+
+function sliderInput(){
+  //print(slider.value());
+  R = sliderR.value();
+  G = sliderG.value();
+  B = sliderB.value();
+  let sendData = R + "," + G + "," + B;
+  send(sendData);
+
+}
+
+function send(data){
+  if(connectedDevice != null){
+  sendText = "sent: "+data; 
+  uBitSend(connectedDevice,data);
+  }else{
+    print("device not connected!");
+  }
+}
+
+function connect() {
+  uBitConnectDevice(uBitEventHandler);
+
+}
+
+function disconnect() {
+  //connectedDevice.close();
+  uBitDisconnect(connectedDevice);
+
+}
+
+function handleData(data){
+  //print(data);
+  recvText = "recv: "+ data.data;
+  brightness = int(data.data);
+  
+}
+
+
+
+// Example event call-back handler
+function uBitEventHandler(reason, device, data) {
+  switch (reason) {
+    case "connected":
+      if(CONSOLE_LOG) print("connected");
+      //      consolePrintln("<b>Connected!</b>")
+      connectedDevice = device;
+      //connectBtn.html("disconnect");
+      //connectBtn.mousePressed(disconnectMicrobit);
+      break
+    case "disconnected":
+      if(CONSOLE_LOG) print("disconnected");
+      //connectBtn.html("connect");
+      //connectBtn.mouseClicked(connectMicrobit);
+      // 
+      //      consolePrintln("<b>Disconnected</b>")
+      connectedDevice = "";
+
+      break
+    case "connection failure":
+      if(CONSOLE_LOG) print("connection failure");
+      //      consolePrintln("<b>Connection Failure</b>")
+      break
+    case "error":
+      if(CONSOLE_LOG) print("error");
+      // consolePrintln("<b>Error</b>")
+      break
+    case "console":
+
+      if(CONSOLE_LOG) print("Console Data: " + data.data)
+      handleData(data);
+      break
+    case "graph-event":
+      if(CONSOLE_LOG) print(`Graph Event:  ${data.data} (for ${data.graph}${data.series.length?" / series "+data.series:""})`)
+      break
+    case "graph-data":
+      if(CONSOLE_LOG) print(`Graph Data: ${data.data} (for ${data.graph}${data.series.length?" / series "+data.series:""})`)
+      break
+  }
+}
